40ad3a32b81975726e72d4597e1dce0425b53311
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-09-10 01:23:19 +0800

    fix bar

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ window.onscroll = function() {topnavFunction()};
 function topnavFunction(){
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("mobile-icon").style.padding = "5px 15px";
-    document.getElementsByClassName("uk-navbar-container")[0].style.padding = "5px";
+    document.getElementsByClassName("uk-navbar-container")[0].style.padding = "2px";
   } else {
     document.getElementById("mobile-icon").style.padding = "10px 30px";  
-    document.getElementsByClassName("uk-navbar-container")[0].style.padding = "10px 30px";  
+    document.getElementsByClassName("uk-navbar-container")[0].style.padding = "5px 15px";  
   }
 }
