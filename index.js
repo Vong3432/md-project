d945c5e2f86fd952dff9d4db16008c6525e1aff9
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-09-10 19:08:25 +0800

    add watches category

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ function topnavFunction(){
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("mobile-icon").style.padding = "5px 15px";
     document.getElementsByClassName("uk-navbar-container")[0].style.padding = "2px";
+    document.getElementsByClassName("uk-navbar-container")[0].style.background = "black";
   } else {
     document.getElementById("mobile-icon").style.padding = "10px 30px";  
-    document.getElementsByClassName("uk-navbar-container")[0].style.padding = "5px 15px";  
+    document.getElementsByClassName("uk-navbar-container")[0].style.padding = "5px 15px";       
   }
 }
 
