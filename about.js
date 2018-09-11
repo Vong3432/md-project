d8ebb404a663a6e282275e43705918deeeecbd72
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-09-12 01:04:47 +0800

    update

--- about.js
+++ about.js
@@ -2,3 +2,18 @@ new Vue({
 	el: '#top-nav',
 
 })
+
+new Vue({
+  el: '#mobile-app',
+  data () {
+    return {
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ],
+      mini: false,
+      right: null
+    }
+  }
+})
