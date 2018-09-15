4980b4e00853bcf9d1cb092824f96c78192f2217
Vong3432 <36991424+Vong3432@users.noreply.github.com>
2018-09-15 21:23:00 +0800

    men.html

---
+++ men.js
@@ -0,0 +1,35 @@
+new Vue({
+  el: '#top-nav',
+  data () {
+    return {      
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard',target:'index.html'},
+        { title: 'About', icon: 'question_answer' }
+
+      ],
+      mini: false,
+      right: null
+    }
+  }
+})
+
+new Vue({
+  el: '#theCarousel',
+  data () {
+    return {
+      items: [
+        {
+          src: 'image/ad1.jpg'
+        },
+        {
+          src: 'image/ad2.jpg'
+        },
+        {
+          src: 'image/ad3.jpg'
+        }        
+      ]
+    }
+  }
+})
+
